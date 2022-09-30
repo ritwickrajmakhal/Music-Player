20e53758f4f55816244f7f68db0778758f082758
RITWICK RAJ MAKHAL <ritwickrajmakhal11@gmail.com>
2022-09-30 23:41:35 +0530

    Calculated sleep time

--- modules.py
+++ modules.py
@@ -1,7 +1,10 @@
 # All features will be written here
+from datetime import datetime
+from datetime import timedelta
 
-def sleep_mode(time):
+def sleep_time(time):
     # write your code here
-
-
-    
+    time = int(time)
+    current_time = datetime.now()
+    future_time = current_time + timedelta(minutes=time)
+    return future_time
