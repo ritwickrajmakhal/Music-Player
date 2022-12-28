f4256936934c7f3f9f5ce0fe79e88572fdb42fbb
RITWICK RAJ MAKHAL <ritwickrajmakhal11@gmail.com>
2022-12-28 11:47:55 +0530

    One test case deleted

--- Music_player_test.py
+++ Music_player_test.py
@@ -1,8 +1,6 @@
-import os
-from time import sleep
 import unittest
 import modules
-import Music_player as mp
+
 class TestModulesMethods(unittest.TestCase):
     def test_setSongIndex(self):
         modules.setSongIndex(0)
@@ -15,11 +13,6 @@ class TestModulesMethods(unittest.TestCase):
     def test_setPath(self):
         expected_path = modules.setPath()
         self.assertEqual(modules.askForPath(),expected_path)  
-    
-    def test_nextSong(self):
-        expected_songIndex = modules.askForSongIndexFile()
-        expected_pos = modules.askForPos()
-        mp.nextSong()
     
 if __name__ == '__main__':
     unittest.main()
